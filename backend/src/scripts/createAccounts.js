c1ff694d52c60690f3cbeaf3172b8c891a9cbb90
John Christian Catolico <johnchristiancatolico70@gmail.com>
2026-09-22 13:20:28 +0800

    some changes and add new midwife Accounts

--- backend/src/scripts/createAccounts.js
+++ backend/src/scripts/createAccounts.js
@@ -119,6 +119,398 @@ const accountsToCreate = [
     barangayId: 2,
     barangayName: 'Caingin'
   },
+    {
+    email: 'midwife.anilao@centralcare.com',
+    password: 'midwife_anilao_123',
+    username: 'Anilao Midwife',
+    role: 'midwife',
+    barangayId: 49,
+    barangayName: 'Anilao'
+  },
+  {
+    email: 'midwife.babatnin@centralcare.com',
+    password: 'midwife_babatnin_123',
+    username: 'Babatnin Midwife',
+    role: 'midwife',
+    barangayId: 50,
+    barangayName: 'Babatnin'
+  },
+  {
+    email: 'midwife.bulihan@centralcare.com',
+    password: 'midwife_bulihan_123',
+    username: 'Bulihan Midwife',
+    role: 'midwife',
+    barangayId: 51,
+    barangayName: 'Bulihan'
+  },
+    {
+    email: 'midwife.atlag@centralcare.com',
+    password: 'midwife_atlag_123',
+    username: 'Atlag Midwife',
+    role: 'midwife',
+    barangayId: 3,
+    barangayName: 'Atlag'
+  },
+  {
+    email: 'midwife.stocristo@centralcare.com',
+    password: 'midwife_stocristo_123',
+    username: 'Sto. Cristo Midwife',
+    role: 'midwife',
+    barangayId: 4,
+    barangayName: 'Sto. Cristo'
+  },
+  {
+    email: 'midwife.balayong@centralcare.com',
+    password: 'midwife_balayong_123',
+    username: 'Balayong Midwife',
+    role: 'midwife',
+    barangayId: 5,
+    barangayName: 'Balayong'
+  },
+  {
+    email: 'midwife.sanjuan@centralcare.com',
+    password: 'midwife_sanjuan_123',
+    username: 'San Juan Midwife',
+    role: 'midwife',
+    barangayId: 6,
+    barangayName: 'San Juan'
+  },
+  {
+    email: 'midwife.storosario@centralcare.com',
+    password: 'midwife_storosario_123',
+    username: 'Sto. Rosario Midwife',
+    role: 'midwife',
+    barangayId: 7,
+    barangayName: 'Sto. Rosario'
+  },
+  {
+    email: 'midwife.calero@centralcare.com',
+    password: 'midwife_calero_123',
+    username: 'Calero Midwife',
+    role: 'midwife',
+    barangayId: 8,
+    barangayName: 'Calero'
+  },
+  {
+    email: 'midwife.bagna@centralcare.com',
+    password: 'midwife_bagna_123',
+    username: 'Bagna Midwife',
+    role: 'midwife',
+    barangayId: 9,
+    barangayName: 'Bagna'
+  },
+  {
+    email: 'midwife.caniogan@centralcare.com',
+    password: 'midwife_caniogan_123',
+    username: 'Caniogan Midwife',
+    role: 'midwife',
+    barangayId: 10,
+    barangayName: 'Caniogan'
+  },
+  {
+    email: 'midwife.catmon@centralcare.com',
+    password: 'midwife_catmon_123',
+    username: 'Catmon Midwife',
+    role: 'midwife',
+    barangayId: 11,
+    barangayName: 'Catmon'
+  },
+  {
+    email: 'midwife.liang@centralcare.com',
+    password: 'midwife_liang_123',
+    username: 'Liang Midwife',
+    role: 'midwife',
+    barangayId: 12,
+    barangayName: 'Liang'
+  },
+  {
+    email: 'midwife.sanagustin@centralcare.com',
+    password: 'midwife_sanagustin_123',
+    username: 'San Agustin Midwife',
+    role: 'midwife',
+    barangayId: 13,
+    barangayName: 'San Agustin'
+  },
+  {
+    email: 'midwife.sangabriel@centralcare.com',
+    password: 'midwife_sangabriel_123',
+    username: 'San Gabriel Midwife',
+    role: 'midwife',
+    barangayId: 14,
+    barangayName: 'San Gabriel'
+  },
+  {
+    email: 'midwife.sanvicente@centralcare.com',
+    password: 'midwife_sanvicente_123',
+    username: 'San Vicente Midwife',
+    role: 'midwife',
+    barangayId: 15,
+    barangayName: 'San Vicente'
+  },
+  {
+    email: 'midwife.stonino@centralcare.com',
+    password: 'midwife_stonino_123',
+    username: 'Sto. Nino Midwife',
+    role: 'midwife',
+    barangayId: 16,
+    barangayName: 'Sto. Nino'
+  },
+  {
+    email: 'midwife.santiago@centralcare.com',
+    password: 'midwife_santiago_123',
+    username: 'Santiago Midwife',
+    role: 'midwife',
+    barangayId: 17,
+    barangayName: 'Santiago'
+  },
+  {
+    email: 'midwife.canalate@centralcare.com',
+    password: 'midwife_canalate_123',
+    username: 'Canalate Midwife',
+    role: 'midwife',
+    barangayId: 18,
+    barangayName: 'Canalate'
+  },
+  {
+    email: 'midwife.bagongbayan@centralcare.com',
+    password: 'midwife_bagongbayan_123',
+    username: 'Bagong Bayan Midwife',
+    role: 'midwife',
+    barangayId: 19,
+    barangayName: 'Bagong Bayan'
+  },
+  {
+    email: 'midwife.balite@centralcare.com',
+    password: 'midwife_balite_123',
+    username: 'Balite Midwife',
+    role: 'midwife',
+    barangayId: 20,
+    barangayName: 'Balite'
+  },
+  {
+    email: 'midwife.cofradia@centralcare.com',
+    password: 'midwife_cofradia_123',
+    username: 'Cofradia Midwife',
+    role: 'midwife',
+    barangayId: 21,
+    barangayName: 'Cofradia'
+  },
+  {
+    email: 'midwife.mabolo@centralcare.com',
+    password: 'midwife_mabolo_123',
+    username: 'Mabolo Midwife',
+    role: 'midwife',
+    barangayId: 22,
+    barangayName: 'Mabolo'
+  },
+  {
+    email: 'midwife.dakila@centralcare.com',
+    password: 'midwife_dakila_123',
+    username: 'Dakila Midwife',
+    role: 'midwife',
+    barangayId: 23,
+    barangayName: 'Dakila'
+  },
+  {
+    email: 'midwife.sbata@centralcare.com',
+    password: 'midwife_sbata_123',
+    username: 'S. Bata Midwife',
+    role: 'midwife',
+    barangayId: 24,
+    barangayName: 'S. Bata'
+  },
+  {
+    email: 'midwife.ligas@centralcare.com',
+    password: 'midwife_ligas_123',
+    username: 'Ligas Midwife',
+    role: 'midwife',
+    barangayId: 25,
+    barangayName: 'Ligas'
+  },
+  {
+    email: 'midwife.bungahan@centralcare.com',
+    password: 'midwife_bungahan_123',
+    username: 'Bungahan Midwife',
+    role: 'midwife',
+    barangayId: 26,
+    barangayName: 'Bungahan'
+  },
+  {
+    email: 'midwife.caliligawan@centralcare.com',
+    password: 'midwife_caliligawan_123',
+    username: 'Caliligawan Midwife',
+    role: 'midwife',
+    barangayId: 27,
+    barangayName: 'Caliligawan'
+  },
+  {
+    email: 'midwife.mambog@centralcare.com',
+    password: 'midwife_mambog_123',
+    username: 'Mambog Midwife',
+    role: 'midwife',
+    barangayId: 28,
+    barangayName: 'Mambog'
+  },
+  {
+    email: 'midwife.masile@centralcare.com',
+    password: 'midwife_masile_123',
+    username: 'Masile Midwife',
+    role: 'midwife',
+    barangayId: 29,
+    barangayName: 'Masile'
+  },
+  {
+    email: 'midwife.matimbo@centralcare.com',
+    password: 'midwife_matimbo_123',
+    username: 'Matimbo Midwife',
+    role: 'midwife',
+    barangayId: 30,
+    barangayName: 'Matimbo'
+  },
+  {
+    email: 'midwife.namayan@centralcare.com',
+    password: 'midwife_namayan_123',
+    username: 'Namayan Midwife',
+    role: 'midwife',
+    barangayId: 31,
+    barangayName: 'Namayan'
+  },
+  {
+    email: 'midwife.pamarawan@centralcare.com',
+    password: 'midwife_pamarawan_123',
+    username: 'Pamarawan Midwife',
+    role: 'midwife',
+    barangayId: 32,
+    barangayName: 'Pamarawan'
+  },
+  {
+    email: 'midwife.panasahan@centralcare.com',
+    password: 'midwife_panasahan_123',
+    username: 'Panasahan Midwife',
+    role: 'midwife',
+    barangayId: 33,
+    barangayName: 'Panasahan'
+  },
+  {
+    email: 'midwife.lugam@centralcare.com',
+    password: 'midwife_lugam_123',
+    username: 'Lugam Midwife',
+    role: 'midwife',
+    barangayId: 34,
+    barangayName: 'Lugam'
+  },
+  {
+    email: 'midwife.look1st@centralcare.com',
+    password: 'midwife_look1st_123',
+    username: 'Look 1st Midwife',
+    role: 'midwife',
+    barangayId: 35,
+    barangayName: 'Look 1st'
+  },
+  {
+    email: 'midwife.pinagbakahan@centralcare.com',
+    password: 'midwife_pinagbakahan_123',
+    username: 'Pinagbakahan Midwife',
+    role: 'midwife',
+    barangayId: 36,
+    barangayName: 'Pinagbakahan'
+  },
+  {
+    email: 'midwife.bangkal@centralcare.com',
+    password: 'midwife_bangkal_123',
+    username: 'Bangkal Midwife',
+    role: 'midwife',
+    barangayId: 37,
+    barangayName: 'Bangkal'
+  },
+  {
+    email: 'midwife.look2nd@centralcare.com',
+    password: 'midwife_look2nd_123',
+    username: 'Look 2nd Midwife',
+    role: 'midwife',
+    barangayId: 38,
+    barangayName: 'Look 2nd'
+  },
+  {
+    email: 'midwife.niugan@centralcare.com',
+    password: 'midwife_niugan_123',
+    username: 'Niugan Midwife',
+    role: 'midwife',
+    barangayId: 39,
+    barangayName: 'Niugan'
+  },
+  {
+    email: 'midwife.santor@centralcare.com',
+    password: 'midwife_santor_123',
+    username: 'Santor Midwife',
+    role: 'midwife',
+    barangayId: 40,
+    barangayName: 'Santor'
+  },
+  {
+    email: 'midwife.taal@centralcare.com',
+    password: 'midwife_taal_123',
+    username: 'Taal Midwife',
+    role: 'midwife',
+    barangayId: 41,
+    barangayName: 'Taal'
+  },
+  {
+    email: 'midwife.mojon@centralcare.com',
+    password: 'midwife_mojon_123',
+    username: 'Mojon Midwife',
+    role: 'midwife',
+    barangayId: 42,
+    barangayName: 'Mojon'
+  },
+  {
+    email: 'midwife.smatanda@centralcare.com',
+    password: 'midwife_smatanda_123',
+    username: 'S. Matanda Midwife',
+    role: 'midwife',
+    barangayId: 43,
+    barangayName: 'S. Matanda'
+  },
+  {
+    email: 'midwife.guinhawa@centralcare.com',
+    password: 'midwife_guinhawa_123',
+    username: 'Guinhawa Midwife',
+    role: 'midwife',
+    barangayId: 44,
+    barangayName: 'Guinhawa'
+  },
+  {
+    email: 'midwife.tikay@centralcare.com',
+    password: 'midwife_tikay_123',
+    username: 'Tikay Midwife',
+    role: 'midwife',
+    barangayId: 45,
+    barangayName: 'Tikay'
+  },
+  {
+    email: 'midwife.sanpablo@centralcare.com',
+    password: 'midwife_sanpablo_123',
+    username: 'San Pablo Midwife',
+    role: 'midwife',
+    barangayId: 46,
+    barangayName: 'San Pablo'
+  },
+  {
+    email: 'midwife.barihan@centralcare.com',
+    password: 'midwife_barihan_123',
+    username: 'Barihan Midwife',
+    role: 'midwife',
+    barangayId: 47,
+    barangayName: 'Barihan'
+  },
+  {
+    email: 'midwife.stmatrinidad@centralcare.com',
+    password: 'midwife_stmatrinidad_123',
+    username: 'Stma. Trinidad Midwife',
+    role: 'midwife',
+    barangayId: 48,
+    barangayName: 'Stma. Trinidad'
+  },
   // Add more midwife accounts here following the same pattern...
 
 ];
